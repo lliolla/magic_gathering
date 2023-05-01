d6143e99573a940221d9373daa7a52e0934eccd1
lliolla <lliolla@hotmail.com>
2023-05-01 09:01:09 +0200

    fix: bug git

--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -1,11 +1,28 @@
-
-
 const Filter = () => {
-    return (
-        <div>
-            filter
-        </div>
-    );
+  return (
+    <div className="filter_container">
+        <span>Filtrer</span>
+      <div className="filter_container-att">
+        <span>Attributs</span>
+        <img src="../../../public/assets/logo/filter-agi-active.png" alt="" />
+        <img src="../../../public/assets/logo/filter-int-active.png" alt="" />
+        <img src="../../../public/assets/logo/filter-str-active.png" alt="" />
+        <img src="../../../public/assets/logo/filter-uni-active.png" alt="" />
+      </div>
+      <div className="filter_container-comp">
+        <span>Complexité</span>
+        <img src="../../../public/assets/logo/filter-diamond.png" alt="" />
+        <img src="../../../public/assets/logo/filter-diamond.png" alt="" />
+        <img src="../../../public/assets/logo/filter-diamond.png" alt="" />
+      </div>
+      <div className="filter_container-search">
+        <img src="../../../public/assets/logo/search.svg" alt="" />
+        <form action="">
+          <input type="text" name="" id="" />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Filter;
--- src/components/gird/Gird.jsx
+++ src/components/gird/Gird.jsx
@@ -2,26 +2,8 @@
 
 const Gird = () => {
     return (
-        <div className="filter_container">
-         <div className="filter_container-att">
-            <span>Attributs</span>
-            <img src="../../../public/assets/logo/filter-agi-active.png" alt="" />
-            <img src="../../../public/assets/logo/filter-int-active.png" alt="" />
-            <img src="../../../public/assets/logo/filter-str-active.png" alt="" />
-            <img src="../../../public/assets/logo/filter-uni-active.png" alt="" />
-         </div>
-         <div className="filter_container-comp">
-            <span>Complexité</span>
-            <img src="../../../public/assets/logo/filter-diamond.png" alt="" />
-            <img src="../../../public/assets/logo/filter-diamond.png" alt="" />
-            <img src="../../../public/assets/logo/filter-diamond.png" alt="" />
-         </div>
-         <div className="filter_container-search">
-         <img src="../../../public/assets/logo/search.svg" alt="" />
-         <form action="">
-            <input type="text" name="" id="" />
-            </form>
-         </div>
+        <div className="gird_container">
+         gird
          </div>     
     )
 }
